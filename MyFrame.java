df0fc5425b36739a3335cbf703d60061b7356905
olosi122 <olosi122@student.liu.se>
2020-03-08 16:29:14 +0100

    O

--- MyFrame.java
+++ MyFrame.java
@@ -1,11 +1,17 @@
 package Projekt.Projekt;
 
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.security.Key;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+//States
+//Buffra bilder
+//Keyboard input för röra sig med piltangenterna
 
 public class MyFrame extends JFrame {
 
@@ -25,9 +31,37 @@ public class MyFrame extends JFrame {
 
         this.setVisible(true);
     }
+
+    private static Clip playASound(String path, boolean repeat) {
+        AudioInputStream audioIn;
+
+        try {
+            audioIn = AudioSystem.getAudioInputStream(new File(path));
+
+            DataLine.Info info = new DataLine.Info(Clip.class, audioIn.getFormat());
+            Clip clip = (Clip) AudioSystem.getLine(info);
+            clip.open(audioIn);
+            if (repeat) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            } else {
+                clip.start();
+            }
+            return clip;
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
     public static void main(String[] args) throws IOException {
         MyFrame frame = new MyFrame();
         String cmd;
+        playASound("Tanks Loop.wav",true);
 
         do {
             //frame.area.updateYourself();
