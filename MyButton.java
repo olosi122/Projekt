196192a749774412fff0d0df056c2ad163489815
olosi122 <olosi122@student.liu.se>
2020-03-31 23:29:43 +0200

    O

---
+++ MyButton.java
@@ -0,0 +1,24 @@
+package Projekt.States;
+
+import javax.swing.*;
+
+import Projekt.Operation.GamePanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class MenuButton extends JButton implements ActionListener {
+
+    //private GamePanel panel;
+
+    public MenuButton(String s) {
+        super(s);
+        addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("You clicked a button");
+
+    }
+}
